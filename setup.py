407af7e64a8eafe53373700cbd8e3a34a25a9362
Evan Palmer <evanp922@gmail.com>
2022-04-22 00:32:07 -0500

    updated setup to include ray package

--- setup.py
+++ setup.py
@@ -4,23 +4,21 @@ import setuptools
 version = "0.0.1"
 
 setup(
-    name='inari',
+    name="inari",
     version=version,
     packages=setuptools.find_packages(),
-    setup_requires=[
-        "setuptools", 
-        "wheel"
-    ],
+    setup_requires=["setuptools", "wheel"],
     install_requires=[
-        'jax>=0.3.5',
-        'flax>=0.4.1',
-        'gin-config>=0.5.0',
-        'rlax>=0.1.2',
-        'dopamine-rl>=4.0.2',
-        'optax>=0.1.1',
-        'tensorflow>=2.8.0',
-        'gym>=0.23.1',
-        'jinja2>=3.1.1',
-        'tqdm>=4.64.0'
+        "jax>=0.3.5",
+        "flax>=0.4.1",
+        "gin-config>=0.5.0",
+        "rlax>=0.1.2",
+        "dopamine-rl>=4.0.2",
+        "optax>=0.1.1",
+        "tensorflow>=2.8.0",
+        "gym>=0.23.1",
+        "jinja2>=3.1.1",
+        "tqdm>=4.64.0",
+        "ray>=1.12.0",
     ],
 )
